13ab2bf20918a74e363b8a2fc322af8615bb9026
James Castle <james.castle@g.austincc.edu>
2022-08-30 16:28:51 -0600

    added save functionality to pasenger counter.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let count = 0;
+saveEl = document.getElementById("save-el");
 
 function increment () {
     console.log ("Clicked");
@@ -6,6 +7,9 @@ function increment () {
     document.getElementById ("count-el").innerText = count;
 }
 
+
 function save () {
+    let saveState = ` ${count} - `;
+    document.getElementById("save-el").innerText += saveState;
     console.log (count);
 }
