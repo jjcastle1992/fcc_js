ed403a513589ed6abd1bd7b76eb80656e1332802
James Castle <james.castle@g.austincc.edu>
2022-08-30 15:36:16 -0600

    update to include basic increment and save button

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-document.getElementById ("count-el").innerText = 5;
+let count = 0;
+
+function increment () {
+    console.log ("Clicked");
+    count++;
+    document.getElementById ("count-el").innerText = count;
+}
+
+function save () {
+    console.log (count);
+}
